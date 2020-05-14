8b765c843ec60a41dd3125a9d64608855c3f873c
wangling <wangling@codeaurora.org>
2020-05-14 14:09:16 +0800

    X00TD: Add RRO Target flag for falcon_64
    Change-Id: I3f69596852c16332aa1a14fc29cb399d4be999da

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-
+# RRO configuration
+TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Power
