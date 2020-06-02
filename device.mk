6e3aba5144c1062d1a64d85964c421f8e4613701
Prabhat774 <mprabhat774@gmail.com>
2020-06-02 09:09:49 +0530

    Revert "X00T: Update thermal service to 2.0"
    This reverts commit f3bc2bd57b2e57fa73404b83b5e45cec7a2d8438.
    
    Update manifest.xml
    
    Update file_contexts

--- device.mk
+++ device.mk
@@ -480,7 +480,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
     thermal.sdm660
 
 PRODUCT_COPY_FILES += \
