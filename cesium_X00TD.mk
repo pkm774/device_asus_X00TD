3e67e42512646a96bc4ef0b4d18663d517c7d0d7
mprabhat774 <mprabhat774@gmail.com>
2020-06-10 17:42:38 +0530

    X00TD: CTS Test 2
    Signed-off-by: mprabhat774 <mprabhat774@gmail.com>

--- cesium_X00TD.mk
+++ cesium_X00TD.mk
@@ -36,7 +36,6 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00TD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_NAME := cesium_X00TD
-PRODUCT_MODEL := ASUS_X00TD
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -44,10 +43,21 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00TD
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys
+
+# The following system and vendor props will be set by vendor init
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.device \
+    ro.product.model \
+    ro.product.name
+
+PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
+    ro.vendor.product.device \
+    ro.vendor.product.model \
+    ro.vendor.product.name
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
