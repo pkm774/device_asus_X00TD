589162bab319980a9ac9db9216a81659cee6715c
mprabhat774 <mprabhat774@gmail.com>
2020-08-08 15:26:20 +0530

    X00TD: Enable system login in enforcing mode
    Retards should read this whole page: https://source.android.com/security/selinux/build
    
    Signed-off-by: mprabhat774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -378,6 +378,7 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.enforcing.rc \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -150,6 +150,16 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+# Force Enforce
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.enforcing.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.enforcing.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
 # Spectrum scripts
 
 include $(CLEAR_VARS)
