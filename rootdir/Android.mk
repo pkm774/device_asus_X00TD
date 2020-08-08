a0441826061187eb54fdee25835fb45887fd3f44
mprabhat774 <mprabhat774@gmail.com>
2020-08-08 15:19:42 +0530

    X00TD: Dropped safailnet as build is not permissive
    Signed-off-by: mprabhat774 <mprabhat774@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -150,14 +150,6 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
 # Spectrum scripts
 
 include $(CLEAR_VARS)
