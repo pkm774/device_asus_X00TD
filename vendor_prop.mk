32c713e83fdcf3456851ccbb69cacdb1013e4f87
Sundong Ahn <sundongahn@google.com>
2020-09-16 16:50:32 +0530

    X00TD: Add system properties for surfaceflinger The configstoreHal will be not used in surfaceflinger. So surfaceflinger will use the system properties instead of configstore. For this, the system properties are added.
    * Add property to enable blur
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -264,3 +264,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.corvus.maintainer=pkm774
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
