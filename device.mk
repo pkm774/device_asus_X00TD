1c3a10dc3560a13cb21791dfead0895cf7341cef
LuK1337 <priv.luk@gmail.com>
2020-10-29 13:24:02 +0100

    X00TD: Enable RRO overlays for lineage-sdk and Snap
    Change-Id: I482f2393e10e90362db2c66b7abb0de837085843
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -412,9 +412,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # RRO configuration
 TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Power
 PRODUCT_PACKAGES += \
