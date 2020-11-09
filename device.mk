b039570bc064926cf4421a03270c82787f607590
mranonymz <mprabhat774@gmail.com>
2020-11-09 21:23:53 +0530

    X00TD: Drop ZenParts and its components
    Signed-off-by: mranonymz <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -369,11 +369,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.spectrum.rc \
-    init.spectrum.sh \
-    init.parallax.rc \
-    init.parallax.sh
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -160,37 +160,3 @@ include $(BUILD_PREBUILT)
 #LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 #include $(BUILD_PREBUILT)
 
-# Spectrum scripts
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.spectrum.rc
-LOCAL_MODULE_TAGS	:= optional
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES	:= etc/init.spectrum.rc
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.spectrum.sh
-LOCAL_MODULE_TAGS	:= optional
-LOCAL_MODULE_CLASS 	:= ETC
-LOCAL_SRC_FILES	:= etc/init.spectrum.sh
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.parallax.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.parallax.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.parallax.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.parallax.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
