cb975228b608ada7acf6cfd08e29a2df2296e419
pkm774 <mprabhat774@gmail.com>
2020-11-28 10:59:37 +0530

    X00TD: Update device build fingerprint to coral november fingeprint

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -168,12 +168,12 @@ void vendor_check_variant()
     if (sys.totalram > 4096ull * 1024 * 1024) {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys";
+            build_fingerprint = "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys";
             product_device = "ASUS_X00T_9";
 
         // Global model
         } else {
-            build_fingerprint = "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys";
+            build_fingerprint = "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys";
             product_device = "ASUS_X00T_3";
         }
 
@@ -181,12 +181,12 @@ void vendor_check_variant()
     } else {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys";
+            build_fingerprint = "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys";
             product_device = "ASUS_X00T_6";
 
         // Global model
         } else {
-            build_fingerprint = "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys";
+            build_fingerprint = "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys";
             product_device = "ASUS_X00T_2";
         }
     }
