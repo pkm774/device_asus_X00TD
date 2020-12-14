6e144da5e7186f1935a4f6ddc729a35dfa164fe6
pkm774 <mprabhat774@gmail.com>
2020-12-14 17:08:58 +0530

    X00TD: Track hals through vendorsetup.sh
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b lineage-17.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+rm -rf hardware/qcom-caf/wlan
+git clone https://android.googlesource.com/platform/hardware/qcom/wlan -b android-11.0.0_r4 hardware/qcom-caf/wlan
+
+rm -rf hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/display
+
