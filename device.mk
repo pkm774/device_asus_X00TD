7acef71028947a36675b098c5d93af1b76fdda5b
Akash98Sky <a98mondal@gmail.com>
2020-12-15 19:53:59 +0530

    X00TD: device: build power stats hal

--- device.mk
+++ device.mk
@@ -349,7 +349,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
