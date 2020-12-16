3e2977517a26c59a02caa48ab34fef06109841e8
Arvind Kumar <karvin@codeaurora.org>
2020-12-16 11:53:35 +0530

    X00TD: Enable Health hal 2.1

--- device.mk
+++ device.mk
@@ -265,8 +265,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+	android.hardware.health@2.1-impl \
+	android.hardware.health@2.1-service \
+	android.hardware.health@2.1-impl.recovery \
 
 # HIDL
 PRODUCT_PACKAGES += \
