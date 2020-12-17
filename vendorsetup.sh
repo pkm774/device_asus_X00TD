d5fd58e1fe432172de2c9a017a26e94e17945a24
pkm774 <mprabhat774@gmail.com>
2020-12-17 18:36:51 +0530

    X00TD: Fetch powe HAL from lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,9 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b lineage-17.1 vendor/qcom/opensource/data-ipa-cfg-mgr
 rm -rf hardware/qcom-caf/wlan
 git clone https://android.googlesource.com/platform/hardware/qcom/wlan -b android-11.0.0_r4 hardware/qcom-caf/wlan
+rm -rf vendor/qcom/opensource/power
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_power -b lineage-18.0 vendor/qcom/opensource/power
+
 
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
