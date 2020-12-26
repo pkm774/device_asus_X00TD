1f70ebc056d0944069c4268d8a2d24abf39bdaac
pkm774 <mprabhat774@gmail.com>
2020-12-26 16:33:47 +0530

    X00TD: device: Build graphics.allocator@3.0 and @4.0
    12-26 14:29:58.440   565   565 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@4.0::IAllocator/default in either framework or device manifest.
    12-26 14:29:58.441  1277  4126 W Gralloc4: allocator 3.x is not supported
    12-26 14:29:58.442   565   565 I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@3.0::IAllocator/default in either framework or device manifest.
    12-26 14:29:58.442  1277  4126 W Gralloc3: allocator 3.x is not supported
    12-26 14:29:58.447  1277  4126 I Gralloc2: Adding additional valid usage bits: 0x0
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
