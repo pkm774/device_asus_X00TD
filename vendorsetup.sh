ea9b7423f0fb4bffc20da2eca5b8466cf1d3ea72
pkm774 <mprabhat774@gmail.com>
2020-12-27 15:56:50 +0530

    X00TD: Fetch upstreamed HALS from pkm774 repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,11 +7,11 @@ git clone https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/
 
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/pkm774/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/pkm774/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/pkm774/android_hardware_qcom_display -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
 
 rm -rf hardware/libhardware
 git clone https://github.com/LineageOS/android_hardware_libhardware -b lineage-18.0 hardware/libhardware
