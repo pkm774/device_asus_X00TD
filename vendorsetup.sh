b426bd3ed9ebfd1b0f7c738e583a6d4089041d64
pkm774 <mprabhat774@gmail.com>
2021-01-08 10:14:47 +0530

    X00TD: Fetch media hal from lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ git clone https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/pkm774/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/pkm774/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
 
