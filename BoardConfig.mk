8b1d5594d96b2324a7453ef8baca812a6cddf808
pkm774 <mprabhat774@gmail.com>
2021-01-16 18:08:13 +0530

    X00TD: Set vendor security date from WW_Phone-17.2017.2012.438-20201203
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -265,7 +265,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-04-05
+VENDOR_SECURITY_PATCH := 2020-12-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
