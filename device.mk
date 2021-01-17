26ff7fd74fc2f83b1efa0f3a4c38475a2bcb4258
ZVNexus <ZVNexus@outlook.com>
2021-01-17 09:52:04 +0530

    X00TD: prebuilt: Add SoundPicker from pixel
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -610,7 +610,8 @@ PRODUCT_PACKAGES += \
 # Prebuilts
 PRODUCT_PACKAGES += \
     GoogleCameraGo \
-    RMPlayer
+    RMPlayer \
+    Sounds
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
