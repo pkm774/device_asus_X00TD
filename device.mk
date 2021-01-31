0e51ba0eec595f0013944c43bace9fe086440ce9
pkm774 <mprabhat774@gmail.com>
2021-01-31 15:58:37 +0530

    X00TD: Update privapp permissions for GcamGo and update sepolicy
    * Drop priv-app permission for OnePlus screen recorder
    * Update camera props from stock A10 X00TD Firmware
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -494,8 +494,7 @@ PRODUCT_PACKAGES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product_privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # QMI
 PRODUCT_PACKAGES += \
