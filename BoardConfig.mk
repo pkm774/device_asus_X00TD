2b1ef56a6c3429c6d273f024be814a38585f04dd
pkm774 <mprabhat774@gmail.com>
2021-06-09 09:49:57 +0530

    X00TD: Impliment stock tpd_gesture for d2tw and gestures
    * Add support for both tpd_gesture and gesture_node
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,7 +232,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
+TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
