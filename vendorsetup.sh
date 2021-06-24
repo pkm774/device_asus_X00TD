72d66719ef0eb5200284d0e0164956d57fd117e7
pkm774 <mprabhat774@gmail.com>
2021-06-24 17:42:35 +0530

    X00TD: Fetch ipacm and wlan from lineage
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,8 @@
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b lineage-17.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
 rm -rf hardware/qcom-caf/wlan
-git clone https://android.googlesource.com/platform/hardware/qcom/wlan -b android-11.0.0_r4 hardware/qcom-caf/wlan
+git clone https://github.com/LineageOS/android_hardware_qcom_wlan -b lineage-18.1-caf hardware/qcom-caf/wlan
+
 rm -rf vendor/qcom/opensource/power
 git clone https://github.com/pkm774/vendor_qcom_opensource_power -b power.legacyopensource.lnx.1.0.r15-rel vendor/qcom/opensource/power
 
