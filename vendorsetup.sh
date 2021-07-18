4a3e4cdc9622208dec7b26ecbb3e85341da0f420
pkm774 <mprabhat774@gmail.com>
2021-07-18 12:34:47 +0530

    X00TD: vendorsetup: Track sbwml's msm8998 HALs
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,9 +8,9 @@ git clone https://github.com/pkm774/vendor_qcom_opensource_power -b power.legacy
 
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/pkm774/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/pkm774/hardware_qcom-caf_audio_msm8998 -b 11 hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/pkm774/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
 git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
 
