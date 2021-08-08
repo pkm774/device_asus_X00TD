ffd8118253a4066f2f332c30f1dd2c3da21b91d7
pkm774 <mprabhat774@gmail.com>
2021-08-08 16:13:44 +0530

    X00TD: Update to redfin August fingerprint
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -207,7 +207,7 @@ void vendor_check_variant()
     
     // Override props based on values set
     property_override_triple("ro.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", 
-    	"google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
+    	"google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
 
 }
 
