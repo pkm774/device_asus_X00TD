7114870b4a3beba74a7c5e7399a60a3f6779a300
pkm774 <mprabhat774@gmail.com>
2021-08-14 17:51:20 +0530

    Revert "X00TD: device: Build QTI thermal 2.0 hal"
    This reverts commit 7d39dd68a48c790931935f6785e3166ee5775647.
    
    - Legacy hal is better then this
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -560,7 +560,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.mock \
+    thermal.sdm660
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
