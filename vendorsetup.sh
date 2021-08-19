64a336f0073b92358ac221122a66921dc2ace7fe
pkm774 <mprabhat774@gmail.com>
2021-08-19 11:42:54 +0000

    X00TD: vendorsetup: Fetch new audio hal
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/pkm774/vendor_qcom_opensource_power -b power.legacy
 
 
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/pkm774/hardware_qcom-caf_audio_msm8998 -b 11 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_audio -b audio-hal.lnx.8.0.r14-rel hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
 git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
