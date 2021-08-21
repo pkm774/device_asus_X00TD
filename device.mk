2d1ecde83b6a70023fcc9672db52671f1e322d4b
matheucomth <matheussouzas65@outlook.com>
2021-08-21 20:19:19 +0530

    X00TD: Uprev to android.hardware.camera.device to 3.6
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -209,7 +209,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
