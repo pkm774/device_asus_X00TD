markzuck420/imported

X00TD: vendorsetup: Remove pixel-charger from vendor
Signed-off-by: pkm774 <mprabhat774@gmail.com>

vendorsetup.sh
@@ -26,3 +26,6 @@ git clone https://github.com/pkm774/packages_apps_SnapdragonCamera -b camera.lnx
 # Private repo vendor/sounds
 rm -rf vendor/sounds
 git clone https://github.com/pkm774/sounds vendor/sounds
+
+# Remove pixel-charger resources from vendor
+rm -rf vendor/descendant/charger

pkm774/device_asus_X00TD

vendorsetup.sh
@@ -22,3 +22,6 @@ git clone https://github.com/LineageOS/android_hardware_libhardware -b lineage-1
 
 rm -rf frameworks/base/data/sounds/google
 git clone https://github.com/pkm774/aosp_sounds frameworks/base/data/sounds/google
+
+# Remove pixel-charger resources from vendor
+rm -rf vendor/corvus/charger