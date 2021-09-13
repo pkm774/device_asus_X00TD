e781cf23a2030d87ff065c00bfc104ab13f523db
pkm774 <mprabhat774@gmail.com>
2021-09-13 13:50:19 +0530

    X00TD: Update redfin fingerprint to September

--- descendant_X00TD.mk
+++ descendant_X00TD.mk
@@ -42,10 +42,10 @@ PRODUCT_NAME := descendant_X00TD
 PRODUCT_MODEL := Zenfone Max Pro (M1)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
