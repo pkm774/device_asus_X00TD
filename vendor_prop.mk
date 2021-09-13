9fdd643022c9431e52cad2094f4bce7f753065d3
Jun Wang <junwan@codeaurora.org>
2021-09-13 13:50:19 +0530

    X00TD: Enable pre-rendering feature

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -217,7 +217,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.perf.scroll_opt=true \
+    ro.vendor.perf.scroll_opt.heavy_app=true
 
 # Enable backpressure for GL comp
 PRODUCT_PROPERTY_OVERRIDES += \
