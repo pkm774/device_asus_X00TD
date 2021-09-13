caa2d3e11907a1fb3f3791ed5d9744d2d98391d8
Hoss Zhou <zhouh@codeaurora.org>
2021-09-13 13:50:19 +0530

    X00TD: Update init.qcom.rc from LA.UM.9.2.r1-03500-SDMxx0.0
    * Build init.qti.can.sh.
    * Revert some previous conflicting commits.
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -473,6 +473,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
+    init.qti.can.sh \
     init.qti.ims.sh \
     move_time_data.sh \
     move_wifi_data.sh \
