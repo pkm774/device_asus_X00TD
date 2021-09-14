5c0f644611ee07656f47d7ceaada220a4bd142c4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-14 16:24:25 +0800

    X00TD: libhidl: Update and Move to Android.bp
    Change-Id: Ia046de0c051aa7533d602714c7d882a079973808
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
