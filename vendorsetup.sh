e531427083f63285ee9afa8b117340ba3848e1a4
pkm774 <mprabhat774@gmail.com>
2021-11-02 10:46:02 +0530

    X00TD: vendorsetup: Update display and media hal repo
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,9 +9,11 @@ git clone https://source.codeaurora.org/quic/la/platform/external/tinycompress -
 rm -rf hardware/qcom-caf/msm8998/audio
 git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_audio -b audio-hal.lnx.8.0.r14-rel hardware/qcom-caf/msm8998/audio
 rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
+git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_media -b video-userspace.lnx.2.10.r10-rel hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
-git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
+git clone https://github.com/pkm774/hardware_qcom-caf_msm8998_display -b display.lnx.3.4.r3-rel hardware/qcom-caf/msm8998/display
+rm -rf vendor/qcom/opensource/commonsys-intf/display
+git clone https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/display-commonsys-intf -b display-sysintf.lnx.1.2.r12-rel vendor/qcom/opensource/commonsys-intf/display
 
 rm -rf hardware/qcom-caf/thermal
 git clone https://github.com/pkm774/android_hardware_qcom_thermal -b aosp-new/android11-qpr3-s1-release hardware/qcom-caf/thermal
