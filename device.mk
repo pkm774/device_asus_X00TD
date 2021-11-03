b0f360324ea1911aae8f77c14a4cee6d3d2316bd
pkm774 <mprabhat774@gmail.com>
2021-11-03 16:06:31 +0530

    X00TD: Build FOSS Camera HAL
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -199,7 +199,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    camera.sdm660
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
