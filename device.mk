6f039f12c38231440f346afb276c20dbc6cfae7f
sbwml <admin@cooluc.com>
2021-11-03 09:12:38 +0530

    X00TD: Build vendor.display.config 1.9
    * Some blobs does not allow the use of vendor.display.config 2.0
      vendor.display.config 1.0 is required. Hence Build vendor.display.config 1.9
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -235,6 +235,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
