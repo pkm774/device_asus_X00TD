49e844c73ed8e396fa3b5f339a295b5df68794c5
pkm774 <mprabhat774@gmail.com>
2022-02-02 21:58:15 +0530

    X00TD: Set dalvik-heap size according to 3gb RAM variant
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -123,12 +123,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Dalvik heap from phone-xhdpi-6144-dalvik-heap.mk
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.5 \
-    dalvik.vm.heapminfree=8m \
-    dalvik.vm.heapmaxfree=32m
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
 
 # Display (SF)
 PRODUCT_PROPERTY_OVERRIDES += \
